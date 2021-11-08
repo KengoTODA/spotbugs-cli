54d6c108387425e854efb18ba0dc15b1d5ba1b0e
Kengo TODA <skypencil@gmail.com>
2021-11-08 19:50:28 +0800

    build: prepare to patch the spotbugs proj

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,29 @@ dependencies {
     kapt("info.picocli:picocli-codegen:$picocliVersion")
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.github.spotbugs:spotbugs:4.4.2")
+    implementation(
+        fileTree("$buildDir/spotbugs/spotbugs-4.4.3-SNAPSHOT/lib").matching {
+            include("*.jar")
+        }
+    )
     implementation("info.picocli:picocli:$picocliVersion")
-    implementation("org.slf4j:slf4j-simple:1.8.0-beta4")
+    implementation("org.slf4j:slf4j-simple:1.8.0-beta4") {
+        isTransitive = false
+    }
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+}
+
+val unzipSpotBugs = tasks.register<Copy>("unzipSpotBugs") {
+    dependsOn(gradle.includedBuild("spotbugs").task(":spotbugs:assemble"))
+
+    from(zipTree(file("../spotbugs/spotbugs/build/distributions/spotbugs-4.4.3-SNAPSHOT.zip")))
+    into("$buildDir/spotbugs")
+}
+
+tasks.compileKotlin {
+    dependsOn(unzipSpotBugs)
 }
 
 kapt {
